f98ec8ee474d4aa73010846e0b16b2c5c0290645
Jeffrey Ness <jeffrey.ness@rackspace.com>
2013-01-10 16:20:02 -0600

    working on the GUI

--- main.py
+++ main.py
@@ -8,31 +8,39 @@ import time
 import Tkinter, ImageTk
 from PIL import Image
 
-
 grid = Grid()
 gayla = Card(**dict(name='Gayla', color='red',
                     upper=2, lower=4, left=4, right=1))
 geezard = Card(**dict(name='Geezard', color='red',
                     upper=2, lower=4, left=4, right=1))
 
-grid.placeCard((0, 0), gayla)
-grid.placeCard((1, 1), geezard)
-grid.placeCard((2, 2), geezard)
+def callback(event):
+    y = int(event.widget.grid_info()['column'])
+    x = int(event.widget.grid_info()['row'])
+    grid.placeCard((x, y), gayla)
+    draw()
+
+def draw():
+    for y in range(3):
+        for x in range(3):
+            pos = (y, x)
+           
+            if grid.getPos(pos):
+                card = grid.getPos(pos)
+                image = Image.open(card.getImage())
+                globals()['p%s%s' % pos ] = ImageTk.PhotoImage(image)
+                label = Tkinter.Label(window, image=globals()['p%s%s' % pos ])
+                label.grid(row=pos[0], column=pos[1])
+            else:
+                image = Image.open('images/empty.png')
+                globals()['p%s%s' % pos ] = ImageTk.PhotoImage(image)
+                label = Tkinter.Label(window, image=globals()['p%s%s' % pos ])
+                label.grid(row=pos[0], column=pos[1])
+                label.bind("<Button-1>", callback)
+
+grid.placeCard((0, 0), geezard)
 
 window = Tkinter.Tk()
-frame = Tkinter.Frame(window, width=279, height=360)
-frame.pack(fill=None, expand=False)
-
-for y in range(3):
-    for x in range(3):
-        pos = (y, x)
-       
-        if grid.getPos(pos):
-            card = grid.getPos(pos)
-            image = Image.open(card.getImage())
-            globals()['p%s%s' % pos ] = ImageTk.PhotoImage(image)
-            label = Tkinter.Label(frame, image=globals()['p%s%s' % pos ])
-            label.place(x=pos[0] * 93, y=pos[1] * 120, width=93, height=120)
-
-        
+window.title("pyTripleTriad")
+draw()
 window.mainloop()
